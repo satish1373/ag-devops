7a1ecfab3c361129a4833c3d742b305d354187f7
satish1373 <satish1373@gmail.com>
2025-09-10 17:47:02 -0500

    :                                     Auto-generated by LangGraph DevOps Autocoder                                     Trace ID: 9828fa38-dcfe-40b7-b42a-256cc7aa97a8                                     Files modified: 2                                     Generation method: ai

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
 import { useState, useEffect } from 'react';
-import ConfirmationDialog from './components/ConfirmationDialog';
+import ComponentName from './components/ComponentName';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [selectedTodo, setSelectedTodo] = useState(null);
-
-  const deleteTodo = (todo) => {
-    setSelectedTodo(todo);
-    setDialogOpen(true);
-  };
-
-  const confirmDelete = () => {
-    setTodos(todos.filter(t => t !== selectedTodo));
-    setDialogOpen(false);
-  };
-
   return (
     <div className='App'>
-      {todos.map(todo => (
-        <div key={todo.id}>
-          <p>{todo.title}</p>
-          <button onClick={() => deleteTodo(todo)}>Delete</button>
-        </div>
-      ))}
-      <ConfirmationDialog
-        isOpen={dialogOpen}
-        message='Are you sure you want to delete this todo?'
-        onConfirm={confirmDelete}
-        onCancel={() => setDialogOpen(false)}
-      />
+      <ComponentName />
     </div>
   );
 }
